87dbeb8db0a489d7a5835835ac9826c3e0249fac
clifton ho <cliftonho311@gmail.com>
2019-10-18 14:07:56 -0700

    redux

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -14,17 +14,42 @@ export default class GoogleAuth extends Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({isSignedIn: this.auth.isSignedIn.get()})
+        this.auth.isSignedIn.listen(this.onAuthChange)
       })
     })
   }
 
+  onAuthChange = () => {
+    this.setState({
+      isSignedIn: this.auth.isSignedIn.get()
+    })
+  }
+
+  onSignInClick = () => {
+    this.auth.signIn()
+  }
+
+  onSignOutClick = () => {
+    this.auth.signOut()
+  }
+
   renderAuthButton() {
-    if(this.state.isSignedIn === false) {
-      return <div>I don't know if we are signed in</div>
+    if(this.state.isSignedIn === null) {
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed In</div>
+      return (
+          <button onClick={this.onSignOutClick}className="ui red google button">
+            <i className="google icon"></i>
+            Sign Out
+          </button>
+        )
     } else {
-      return <div>nOpe, not signed in</div>
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      )
     }
   }
 
